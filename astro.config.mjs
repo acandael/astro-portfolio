01d2be1bc853ad4c500c3c58faeb1a82aba67367
Anthony Candaele <anthonycandaele@Anthonys-Mac-Studio.local>
2024-02-24 13:46:13 +0100

    lesson 65

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  prefetch: {
+    prefetchAll: true,
+  },
+  site: "https://stargazers.club",
+  integrations: [icon(), sitemap()],
+  output: "hybrid",
+  adapter: netlify(),
+});
