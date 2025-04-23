c574d43232602c2a950b8f7beba82c7d1f31212c
Anthony Candaele <info@anthonycandaele.com>
2025-04-23 15:43:49 +0200

    implement blog

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,32 @@
-import { file } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { file, glob } from "astro/loaders";
+import { defineCollection, reference, z } from "astro:content";
+
+const blog = defineCollection({
+  loader: glob({
+    pattern: "src/content/blog/**/*.md",
+  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().max(65),
+      slug: z.string(),
+      description: z.string().max(160),
+      image: image(),
+      pubDate: z.date(),
+      isDraft: z.boolean().optional(),
+      author: reference("authors"),
+    }),
+});
+
+const authors = defineCollection({
+  loader: async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    return data.map((p: any) => ({
+      id: p.name,
+      name: p.name,
+    }));
+  },
+});
 
 const features = defineCollection({
   loader: file("src/content/features.json"),
@@ -10,4 +37,14 @@ const features = defineCollection({
   }),
 });
 
-export const collections = { features };
+const projects = defineCollection({
+  loader: file("src/content/projects.yaml"),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().max(65),
+      href: z.string().url(),
+      image: image(),
+    }),
+});
+
+export const collections = { features, projects, blog, authors };
